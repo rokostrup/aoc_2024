c424cdbd621e443b0271beac8d8fb0d4ee5fe3d8
Aleksander Østrup <aleksander@rokoko.com>
2024-12-07 13:37:43 +0100

    Solve d6p2. Brute force but with a few optimizations. Solution found in ~10s. Very parallelizable so could be fun to try to use concurrency here.

--- d6/src/main.rs
+++ d6/src/main.rs
@@ -1,13 +1,16 @@
-use std::borrow::Borrow;
-use std::clone;
 use std::collections::HashSet;
-use std::ops::Add;
-use std::{fs, iter::Map};
+use std::fs;
 
 #[derive(PartialEq, Eq, Debug, Clone, Copy, Hash)]
 struct Point {
     x: i32,
     y: i32,
+}
+
+#[derive(PartialEq, Eq, Debug, Clone, Copy, Hash)]
+struct Arrow {
+    p: Point,
+    dir: Point,
 }
 
 impl ::core::ops::Add for Point {
@@ -25,35 +28,52 @@ type Obstructions = Vec<Point>;
 type Direction = Point;
 type MapSize = Point;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Guard {
     pos: Point,
     dir: Direction,
     obs: Obstructions,
     m_sz: MapSize,
+    new_ob: Option<Point>,
+    vis_w_dir: HashSet<Arrow>,
+}
+
+enum WalkResult {
+    Exit,
+    LoopDetected,
+    TookStep,
 }
 
 impl Guard {
-    fn walk(&mut self) -> bool {
+    fn walk(&mut self) -> WalkResult {
         let new_pos = self.pos + self.dir;
 
-        if new_pos.x >= self.m_sz.x || new_pos.y >= self.m_sz.y {
-            println!("Left map at {:?}", new_pos);
-            return false; // left the map
+        if new_pos.x >= self.m_sz.x || new_pos.y >= self.m_sz.y || new_pos.x < 0 || new_pos.y < 0 {
+            return WalkResult::Exit;
+        }
+
+        let mut new_hit = false;
+        if let Some(p) = self.new_ob {
+            new_hit = p == new_pos;
         }
 
         // still on the map - hit an obstacle?
-        if self.obs.contains(&new_pos) {
-            println!("Hit obstruction at {:?}", new_pos);
+        if new_hit || self.obs.contains(&new_pos) {
             self.rotate_right();
             return self.walk();
         }
 
-        // not hit - update position
-        println!("Walkted to {:?}", new_pos);
         self.pos = new_pos;
 
-        true
+        // cache visited places (with a direction!) for faster loop detection
+        if !self.vis_w_dir.insert(Arrow {
+            p: new_pos,
+            dir: self.dir,
+        }) {
+            return WalkResult::LoopDetected;
+        }
+
+        return WalkResult::TookStep;
     }
 
     fn rotate_right(&mut self) {
@@ -80,20 +100,8 @@ fn create_obs_from_str(row: usize, c_str: &str) -> Obstructions {
         .collect()
 }
 
-fn parse_input() -> (Guard) {
+fn parse_input() -> Guard {
     let s = fs::read_to_string("input.txt").expect("Should have been able to read the file");
-    //     let s = String::from(
-    //         r"....#.....
-    // .........#
-    // ..........
-    // ..#.......
-    // .......#..
-    // ..........
-    // .#..^.....
-    // ........#.
-    // #.........
-    // ......#...",
-    //     );
 
     // we extract all the positions of blocks
     let lines = s.split_ascii_whitespace();
@@ -124,19 +132,55 @@ fn parse_input() -> (Guard) {
             x: cols as i32,
             y: rows as i32,
         },
+        new_ob: None,
+        vis_w_dir: HashSet::new(),
     }
 }
+
+// loop detection - cache guards position and direction
+// if already present, loop detected -> abort
+fn loop_detected(p: Point, mut g: Guard) -> bool {
+    // add new position to check
+    g.new_ob = Some(p);
+    g.vis_w_dir.clear();
+
+    loop {
+        match g.walk() {
+            WalkResult::Exit => return false,
+            WalkResult::TookStep => continue,
+            WalkResult::LoopDetected => return true,
+        }
+    }
+}
+
+// using the following optimzation tricks
+// object must be where:
+// the guards original path
+fn cnt_loop_locations(candidates: &HashSet<Point>, g: &mut Guard) -> u32 {
+    candidates
+        .iter()
+        .filter(|p| loop_detected(**p, g.clone()))
+        .count() as u32
+}
+
+// compile with optimization!
 fn main() {
     let mut g = parse_input();
+    let mut g_copy = g.clone();
 
     let mut visited = HashSet::new();
-    visited.insert(g.pos); // start position
 
-    while g.walk() {
+    loop {
         visited.insert(g.pos);
+        match g.walk() {
+            WalkResult::Exit => break,
+            WalkResult::TookStep => continue,
+            WalkResult::LoopDetected => panic!("Should not happen here!"),
+        }
     }
 
-    println!("{}", visited.len());
-
-    println!("Map size = {:?}", g.m_sz);
+    println!("p1 = {}", visited.len());
+    // the start position is not a candidate - remove
+    visited.remove(&g_copy.pos);
+    println!("p2 = {}", cnt_loop_locations(&visited, &mut g_copy));
 }
